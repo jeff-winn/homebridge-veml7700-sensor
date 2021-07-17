672f1f44b086d30974ac877c5a0cb7871674892c
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-16 20:38:24 -0400

    Initial check-in.

---
+++ src/accessory.ts
@@ -0,0 +1,68 @@
+import { 
+  AccessoryConfig, 
+  AccessoryPlugin, 
+  API, 
+  CharacteristicEventTypes, 
+  CharacteristicGetCallback, 
+  CharacteristicSetCallback, 
+  CharacteristicValue, 
+  HAP, 
+  Logging, 
+  Service 
+} from "homebridge";
+
+let hap: HAP;
+
+/*
+ * Initializer function called when the plugin is loaded.
+ */
+export = (api: API) => {
+  hap = api.hap;
+  api.registerAccessory("Wireless Rain-Clik Receiver", RainClikSensor);
+};
+
+class RainClikSensor implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+  private contactDetected = false;
+
+  private readonly sensorService: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+
+    this.sensorService = new hap.Service.ContactSensor(this.name);
+    this.sensorService.getCharacteristic(hap.Characteristic.ContactSensorState)
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.info("Current state of the sensor was returned: " + (this.contactDetected ? "DETECTED": "NOT_DETECTED"));
+        callback(undefined, this.contactDetected);
+      });
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "Hunter Industries")
+      .setCharacteristic(hap.Characteristic.Model, "WR-CLIK-R");
+
+    log.info("Sensor finished initializing!");
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.sensorService,
+    ];
+  }
+}
