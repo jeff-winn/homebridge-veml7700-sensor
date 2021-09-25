62230d47715124a188544ff7ae0988628816ba72
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-09-24 23:43:54 -0400

    Refactoring.

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,9 +1,89 @@
-import { API } from "homebridge";
-import { Veml7700LightSensor } from './sensors/lightSensor';
+import { 
+  AccessoryConfig, 
+  AccessoryPlugin, 
+  API, 
+  CharacteristicEventTypes, 
+  CharacteristicGetCallback, 
+  HAP, 
+  Logging, 
+  Service 
+} from "homebridge";
 
 /*
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  api.registerAccessory("Adafruit VEML7700 Light Sensor", Veml7700LightSensor);
+  api.registerAccessory("Adafruit VEML7700", Veml7700Accessory);
 };
+
+class Veml7700Accessory implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+  private readonly hap: HAP;
+
+  private currentValue: number = 0;
+  private contactDetected: boolean = false;
+  
+  private readonly contactService?: Service;
+  private readonly lightService?: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+    this.hap = api.hap;
+
+    if (config.contactSensor) {
+      this.contactService = new this.hap.Service.ContactSensor(this.name + " Contact Sensor");
+      this.contactService.getCharacteristic(this.hap.Characteristic.ContactSensorState)
+        .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+          log.info("Current state of the sensor was returned: " + (this.contactDetected ? "DETECTED": "NOT_DETECTED"));
+          callback(undefined, this.contactDetected);
+        });  
+    }
+
+    if (config.lightSensor) {
+      this.lightService = new this.hap.Service.LightSensor(this.name + "Light Sensor");
+      this.lightService.getCharacteristic(this.hap.Characteristic.CurrentAmbientLightLevel)
+        .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+          this.currentValue = 0.0001;        
+          log.info("Current state of the sensor was returned: " + this.currentValue);
+
+          callback(undefined, this.currentValue);
+        });
+    }
+
+    this.informationService = new this.hap.Service.AccessoryInformation()
+      .setCharacteristic(this.hap.Characteristic.Manufacturer, "Adafruit Industries")
+      .setCharacteristic(this.hap.Characteristic.Model, "VEML7700");
+
+    log.info("Sensor finished initializing!");
+  }
+  
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    let services: Service[] = [];
+    services.push(this.informationService);
+    
+    if (this.contactService) {
+      services.push(this.contactService);
+    }
+
+    if (this.lightService) {
+      services.push(this.lightService);
+    }
+
+    return services;
+  }
+}
