d086c3966346a8369a0cfd901fcda16cc9b2b5e7
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-07-03 11:55:05 -0400

    Refactoring.

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,155 +1,70 @@
 import {
     AccessoryConfig, AccessoryPlugin, API, APIEvent, Logging, Service
 } from 'homebridge';
+import { LightSensorClientImpl } from './clients/lightSensorClient';
+import { TimerImpl } from './primitives/timer';
 
 import { AccessoryInformation, AccessoryInformationImpl } from './services/accessoryInformation';
 import { RainSensor, RainSensorImpl } from './services/rainSensor';
 
 export interface Veml7700AccessoryConfig extends AccessoryConfig {
-  pollingInterval: number;
-  url: string;
-  minimum: number | undefined;
+    pollingInterval: number;
+    url: string;
+    minimum: number | undefined;
 }
 
 export class Veml7700Accessory implements AccessoryPlugin {
     private readonly config: Veml7700AccessoryConfig;
 
-    private accessoryInformation: AccessoryInformation;
-    private rainSensor: RainSensor;
-
-    // private readonly name: string;
-    // private readonly hap: HAP;
-    // private readonly pollingInterval: number;
-    // private readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
-  
-    // private readonly sensorService: Service;
-    // private readonly informationService: Service;
-    // private readonly client: LightSensorClient;
-    
-    // private contactState: number;
+    private accessoryInformation?: AccessoryInformation;
+    private rainSensor?: RainSensor;
   
     public constructor(private log: Logging, c: AccessoryConfig, private api: API) {
         this.config = c as Veml7700AccessoryConfig;
 
-        this.accessoryInformation = new AccessoryInformationImpl(this, this.api);
-        this.accessoryInformation.init();
-
-        this.rainSensor = new RainSensorImpl(this, this.api);
-        this.rainSensor.init();
-
         api.on(APIEvent.DID_FINISH_LAUNCHING, this.onFinishedLaunching.bind(this));
         api.on(APIEvent.SHUTDOWN, this.onShutdown.bind(this));
-
-        // this.name = this.config.name;
-        // this.hap = api.hap;
-        // this.pollingInterval = this.config.pollingInterval * 1000;
-        
-        // this.client = new LightSensorClientImpl(this.config.url);
-        // this.contactState = this.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
-
-        // this.sensorService = new this.hap.Service.ContactSensor(this.name);
-        // this.sensorService.getCharacteristic(this.Characteristic.ContactSensorState)
-        //     .on(CharacteristicEventTypes.GET, this.onContactSensorGetCallback.bind(this));
-
-        // this.informationService = new this.hap.Service.AccessoryInformation()
-        //     .setCharacteristic(this.Characteristic.Manufacturer, 'Jeff Winn')
-        //     .setCharacteristic(this.Characteristic.Model, 'VEML7700');
-
-        // log.info('Sensor finished initializing!');
     }
 
     private onFinishedLaunching(): void {
-        this.rainSensor.start();
+        this.rainSensor?.start();
+
+        // log.info('Sensor finished initializing!');
     }
 
     private onShutdown(): void {
         this.rainSensor?.stop();
     }
-
-    // private beginPollingContactSensorState(): void {
-    //     setTimeout(this.monitorContactSensorState.bind(this), this.pollingInterval);
-    //     this.log.debug('Restarted timer.');
-    // }
   
-    // private async monitorContactSensorState(): Promise<void> {
-    //     try {
-    //         const newValue = await this.checkContactSensorState();
-    //         if (newValue !== this.contactState) {
-    //             this.contactState = newValue;
-    //             this.log.info(`Contact: ${this.contactState === this.Characteristic.ContactSensorState.CONTACT_DETECTED ? 
-    //                 'DETECTED' : 'NOT_DETECTED'}@${newValue}`);
-
-    //             this.sensorService.getCharacteristic(this.Characteristic.ContactSensorState).updateValue(this.contactState);
-    //         }  
-    //     } catch (err) {
-    //         this.log.error('An error occurred while checking the sensor.', err);
-    //     } finally {
-    //         this.beginPollingContactSensorState();
-    //     }
-    // }
-    
-    // private onContactSensorGetCallback(callback: CharacteristicGetCallback): void {
-    //     callback(undefined, this.contactState);  
-    // }
-  
-    // private async checkContactSensorState(): Promise<number> {
-    //     this.log.debug('Checking the state of the sensor...');
-  
-    //     let result = false;
-  
-    //     const data = await this.client.inspect();
-    //     if (data === undefined) {
-    //         this.log.warn('State of the sensor was not returned.');
-    //     } else {
-    //         const lux = data.lux;
-    //         this.log.debug(`Lux: ${lux}`);
-  
-    //         if (this.config.minimum === undefined) {
-    //             result = lux > 0;
-    //         } else {
-    //             result = lux >= this.config.minimum;
-    //         }      
-    //     }
-  
-    //     if (result) {
-    //         return this.Characteristic.ContactSensorState.CONTACT_DETECTED;
-    //     }
-  
-    //     return this.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
-    // }
-  
-    // /*
-    //  * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-    //  * Typical this only ever happens at the pairing process.
-    //  */
-    // public identify(): void {
-    //     this.log('Identify!');
-    // }
-  
-    /*
-     * This method is called directly after creation of this instance.
-     * It should return all services which should be added to the accessory.
-     */
+    /* While named get services, this is actually what initalizes the
+       accessory given that it's called before any events get executed. */
     public getServices(): Service[] {
         const result: Service[] = [];
 
-        const accessoryInformation = this.accessoryInformation.getUnderlyingService();
-        if (accessoryInformation !== undefined) {
-            result.push(accessoryInformation);
+        this.accessoryInformation = this.createAccessoryInformation();
+        this.accessoryInformation.init();
+
+        const accessoryInformationService = this.accessoryInformation.getUnderlyingService();
+        if (accessoryInformationService !== undefined) {
+            result.push(accessoryInformationService);
         }
 
-        const rainSensor = this.rainSensor.getUnderlyingService();
-        if (rainSensor !== undefined) {
-            result.push(rainSensor);
+        this.rainSensor = this.createRainSensor();
+        this.rainSensor.init();
+
+        const rainSensorService = this.rainSensor.getUnderlyingService();        
+        if (rainSensorService !== undefined) {
+            result.push(rainSensorService);
         }
 
-        return result;
-        
-        // this.beginPollingContactSensorState();
-  
-        // return [
-        //     this.informationService,
-        //     this.sensorService
-        // ];
+        return result;        
+    }
+
+    protected createAccessoryInformation(): AccessoryInformation {
+        return new AccessoryInformationImpl(this, this.api);
+    }
+
+    protected createRainSensor(): RainSensor {
+        return new RainSensorImpl(this.config.name, new TimerImpl(), new LightSensorClientImpl(this.config.url), this, this.api);
     }
 }
--- src/services/rainSensor.ts
+++ src/services/rainSensor.ts
@@ -1,4 +1,6 @@
-import { Service } from 'homebridge';
+import { AccessoryPlugin, API, Characteristic, Service } from 'homebridge';
+import { LightSensorClient } from '../clients/lightSensorClient';
+import { Timer } from '../primitives/timer';
 import { AbstractAccessoryService } from './homebridge/abstractAccessoryService';
 
 export interface RainSensor {
@@ -12,19 +14,90 @@ export interface RainSensor {
 }
 
 export class RainSensorImpl extends AbstractAccessoryService implements RainSensor {
+    private sensorService?: Service;
+    private contactState?: Characteristic;
+
+    public constructor(private name: string, private timer: Timer, private client: LightSensorClient, 
+        protected accessory: AccessoryPlugin, protected api: API) { 
+        super(accessory, api);
+    }    
+
     public getUnderlyingService(): Service | undefined {
-        return undefined;
+        return this.sensorService;
     }
 
-    public init(): void {
-        // Do nothing.
+    public init(): void {        
+        this.sensorService = this.createService();
+        this.contactState = this.sensorService.getCharacteristic(this.Characteristic.ContactSensorState);
+        
+        // this.client = new LightSensorClientImpl(this.config.url);
+
+        // this.pollingInterval = this.config.pollingInterval * 1000;
+    }
+
+    protected createService(): Service {
+        return new this.Service.ContactSensor(this.name);
     }
 
     public start(): void {
         // Do nothing.
+
+        // this.beginPollingContactSensorState();
     }
 
     public stop(): void {
         // Do nothing.
     }
+
+    // private beginPollingContactSensorState(): void {
+    //     setTimeout(this.monitorContactSensorState.bind(this), this.pollingInterval);
+    //     this.log.debug('Restarted timer.');
+    // }
+  
+    // private async monitorContactSensorState(): Promise<void> {
+    //     try {
+    //         const newValue = await this.checkContactSensorState();
+    //         if (newValue !== this.contactState) {
+    //             this.contactState = newValue;
+    //             this.log.info(`Contact: ${this.contactState === this.Characteristic.ContactSensorState.CONTACT_DETECTED ? 
+    //                 'DETECTED' : 'NOT_DETECTED'}@${newValue}`);
+
+    //             this.sensorService.getCharacteristic(this.Characteristic.ContactSensorState).updateValue(this.contactState);
+    //         }  
+    //     } catch (err) {
+    //         this.log.error('An error occurred while checking the sensor.', err);
+    //     } finally {
+    //         this.beginPollingContactSensorState();
+    //     }
+    // }
+    
+    // private onContactSensorGetCallback(callback: CharacteristicGetCallback): void {
+    //     callback(undefined, this.contactState);  
+    // }
+  
+    // private async checkContactSensorState(): Promise<number> {
+    //     this.log.debug('Checking the state of the sensor...');
+  
+    //     let result = false;
+  
+    //     const data = await this.client.inspect();
+    //     if (data === undefined) {
+    //         this.log.warn('State of the sensor was not returned.');
+    //     } else {
+    //         const lux = data.lux;
+    //         this.log.debug(`Lux: ${lux}`);
+  
+    //         if (this.config.minimum === undefined) {
+    //             result = lux > 0;
+    //         } else {
+    //             result = lux >= this.config.minimum;
+    //         }      
+    //     }
+  
+    //     if (result) {
+    //         return this.Characteristic.ContactSensorState.CONTACT_DETECTED;
+    //     }
+  
+    //     return this.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
+    // }
 }
