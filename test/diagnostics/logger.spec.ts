38fe80c93f388fff23fd88cee9b3f029a30f4c8f
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-07-03 14:12:27 -0400

    Fixing unit tests.

--- test/diagnostics/logger.spec.ts
+++ test/diagnostics/logger.spec.ts
@@ -5,26 +5,22 @@ import * as settings from '../../src/settings';
 import { HomebridgeImitationLogger } from '../../src/diagnostics/logger';
 import { ConsoleWrapper } from '../../src/diagnostics/primitives/consoleWrapper';
 import { Environment } from '../../src/primitives/environment';
-import { Localization } from '../../src/primitives/localization';
 
 describe('HomebridgeImitationLogger', () => {
     let env: Mock<Environment>;
     let console: Mock<ConsoleWrapper>;
-    let locale: Mock<Localization>;
 
     let target: HomebridgeImitationLogger;
 
     beforeEach(() => {
         env = new Mock<Environment>();
         console = new Mock<ConsoleWrapper>();
-        locale = new Mock<Localization>();
 
-        target = new HomebridgeImitationLogger(env.object(), 'platform', 'my instance', console.object(), locale.object());
+        target = new HomebridgeImitationLogger(env.object(), 'platform', 'my instance', console.object());
     });
 
     it('should log the debug to stdout when enabled via plugin id', () => {
         env.setup(o => o.getDebugEnvironmentVariable()).returns(settings.PLUGIN_ID);
-        locale.setup(o => o.format('hello')).returns('hello');
 
         console.setup(o => o.stdout(It.IsAny())).returns(undefined);
 
@@ -37,8 +33,6 @@ describe('HomebridgeImitationLogger', () => {
         env.setup(o => o.getDebugEnvironmentVariable()).returns(settings.PLUGIN_ID);
 
         console.setup(o => o.stdout(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
-        locale.setup(o => o.format('hello again')).returns('hello again');
 
         target.debug('hello');
         target.debug('hello again');
@@ -51,7 +45,6 @@ describe('HomebridgeImitationLogger', () => {
         env.setup(o => o.getDebugEnvironmentVariable()).returns('*');
 
         console.setup(o => o.stdout(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
 
         target.debug('hello');
         
@@ -62,7 +55,6 @@ describe('HomebridgeImitationLogger', () => {
         env.setup(o => o.getDebugEnvironmentVariable()).returns('not the plugin id');
 
         console.setup(o => o.stdout(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
 
         target.debug('hello');
         
@@ -71,7 +63,6 @@ describe('HomebridgeImitationLogger', () => {
 
     it('should log the information to stdout', () => {
         console.setup(o => o.stdout(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
         
         target.info('hello');
         
@@ -80,7 +71,6 @@ describe('HomebridgeImitationLogger', () => {
 
     it('should log the warning to stderr', () => {
         console.setup(o => o.stderr(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
 
         target.warn('hello');
         
@@ -89,7 +79,6 @@ describe('HomebridgeImitationLogger', () => {
 
     it('should log the error to stderr', () => {
         console.setup(o => o.stderr(It.IsAny())).returns(undefined);
-        locale.setup(o => o.format('hello')).returns('hello');
 
         target.error('hello');
         
